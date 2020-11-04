294a04221a6922707f43b9244ab191c023170482
Szilárd Pfeiffer <szilard.pfeiffer@balasys.hu>
2020-11-04 11:59:42 +0100

    pep8: fixed indentation of multiline expressions issues (E124, E128)

--- policy.py
+++ policy.py
@@ -26,90 +26,101 @@ from Zorp.Pop3 import *
 from Zorp.Smtp import *
 
 
-InetZone(name="clients",
-         addrs=["172.16.10.0/23", ],
-         inbound_services=["*"],
-         outbound_services=["*"]
-        )
+InetZone(
+    name="clients",
+    addrs=["172.16.10.0/23", ],
+    inbound_services=["*"],
+    outbound_services=["*"]
+)
 
 
-InetZone(name="servers",
-         addrs=["172.16.20.0/23", ],
-         inbound_services=["*"],
-         outbound_services=["*"]
-        )
+InetZone(
+    name="servers",
+    addrs=["172.16.20.0/23", ],
+    inbound_services=["*"],
+    outbound_services=["*"]
+)
 
 
-InetZone(name="servers.audit",
-         addrs=["172.16.21.1/32", ],
-         inbound_services=["*"],
-         outbound_services=["*"],
-         admin_parent="servers"
-        )
+InetZone(
+    name="servers.audit",
+    addrs=["172.16.21.1/32", ],
+    inbound_services=["*"],
+    outbound_services=["*"],
+    admin_parent="servers"
+)
 
 
-InetZone(name="servers.stack_clamav",
-         addrs=["172.16.21.5/32", ],
-         inbound_services=["*"],
-         outbound_services=["*"],
-         admin_parent="servers"
-        )
+InetZone(
+    name="servers.stack_clamav",
+    addrs=["172.16.21.5/32", ],
+    inbound_services=["*"],
+    outbound_services=["*"],
+    admin_parent="servers"
+)
 
 
-InetZone(name="servers.smtp_starttls",
-         addrs=["172.16.21.9/32", ],
-         inbound_services=["*"],
-         outbound_services=["*"],
-         admin_parent="servers"
-        )
+InetZone(
+    name="servers.smtp_starttls",
+    addrs=["172.16.21.9/32", ],
+    inbound_services=["*"],
+    outbound_services=["*"],
+    admin_parent="servers"
+)
 
 
-InetZone(name="servers.smtp_one_sided_ssl",
-         addrs=["172.16.21.13/32", ],
-         inbound_services=["*"],
-         outbound_services=["*"],
-         admin_parent="servers"
-        )
+InetZone(
+    name="servers.smtp_one_sided_ssl",
+    addrs=["172.16.21.13/32", ],
+    inbound_services=["*"],
+    outbound_services=["*"],
+    admin_parent="servers"
+)
 
 
-InetZone(name="servers.http_stack_cat",
-         addrs=["172.16.21.17/32", ],
-         inbound_services=["*"],
-         outbound_services=["*"],
-         admin_parent="servers"
-        )
+InetZone(
+    name="servers.http_stack_cat",
+    addrs=["172.16.21.17/32", ],
+    inbound_services=["*"],
+    outbound_services=["*"],
+    admin_parent="servers"
+)
 
 
-InetZone(name="servers.http_stack_tr",
-         addrs=["172.16.21.21/32", ],
-         inbound_services=["*"],
-         outbound_services=["*"],
-         admin_parent="servers"
-        )
+InetZone(
+    name="servers.http_stack_tr",
+    addrs=["172.16.21.21/32", ],
+    inbound_services=["*"],
+    outbound_services=["*"],
+    admin_parent="servers"
+)
 
 
-InetZone(name="servers.http_header_replace",
-         addrs=["172.16.21.25/32", ],
-         inbound_services=["*"],
-         outbound_services=["*"],
-         admin_parent="servers"
-        )
+InetZone(
+    name="servers.http_header_replace",
+    addrs=["172.16.21.25/32", ],
+    inbound_services=["*"],
+    outbound_services=["*"],
+    admin_parent="servers"
+)
 
 
-InetZone(name="servers.http_url_filter",
-         addrs=["172.16.21.29/32", ],
-         inbound_services=["*"],
-         outbound_services=["*"],
-         admin_parent="servers"
-        )
+InetZone(
+    name="servers.http_url_filter",
+    addrs=["172.16.21.29/32", ],
+    inbound_services=["*"],
+    outbound_services=["*"],
+    admin_parent="servers"
+)
 
 
-InetZone(name="servers.plug",
-         addrs=["172.16.21.33/32", ],
-         inbound_services=["*"],
-         outbound_services=["*"],
-         admin_parent="servers"
-        )
+InetZone(
+    name="servers.plug",
+    addrs=["172.16.21.33/32", ],
+    inbound_services=["*"],
+    outbound_services=["*"],
+    admin_parent="servers"
+)
 
 
 class HttpProxyHeaderReplace(HttpProxy):
@@ -143,9 +154,9 @@ class SmtpProxyStartTls(SmtpProxy):
         self.ssl.client_connection_security = SSL_ACCEPT_STARTTLS
         self.ssl.client_verify_type = SSL_VERIFY_OPTIONAL_UNTRUSTED
         self.ssl.client_keypair_files  = (
-                           "/etc/ssl/certs/ssl-cert-snakeoil.pem",
-                           "/etc/ssl/private/ssl-cert-snakeoil.key"
-                                              )
+            "/etc/ssl/certs/ssl-cert-snakeoil.pem",
+            "/etc/ssl/private/ssl-cert-snakeoil.key"
+        )
         self.ssl.server_verify_type = SSL_VERIFY_OPTIONAL_UNTRUSTED
 
 
@@ -187,163 +198,195 @@ class HttpsProxyKeybridge(HttpProxy):
 
 def zorp_instance():
     #http services
-    Service(name='service_http_transparent',
-            proxy_class=HttpProxy,
-            router=TransparentRouter()
+    Service(
+        name='service_http_transparent',
+        proxy_class=HttpProxy,
+        router=TransparentRouter()
     )
-    Service(name="service_http_transparent_directed",
-            proxy_class=HttpProxy,
-            router=DirectedRouter(dest_addr=SockAddrInet('172.16.20.254', 80))
+    Service(
+        name="service_http_transparent_directed",
+        proxy_class=HttpProxy,
+        router=DirectedRouter(dest_addr=SockAddrInet('172.16.20.254', 80))
     )
-    Service(name="service_http_transparent_header_replace",
+    Service(
+        name="service_http_transparent_header_replace",
         proxy_class=HttpProxyHeaderReplace,
         router=TransparentRouter()
     )
-    Service(name="service_http_transparent_url_filter",
+    Service(
+        name="service_http_transparent_url_filter",
         proxy_class=HttpProxyUrlFilter,
         router=TransparentRouter()
     )
-    Service(name="service_http_nontransparent_inband",
-            proxy_class=HttpProxyNonTransparent,
-            router=InbandRouter(forge_port=TRUE)
+    Service(
+        name="service_http_nontransparent_inband",
+        proxy_class=HttpProxyNonTransparent,
+        router=InbandRouter(forge_port=TRUE)
     )
 
     #plug service
-    Service(name="service_plug",
+    Service(
+        name="service_plug",
         proxy_class=PlugProxy,
         router=TransparentRouter()
     )
 
     #https services
-    Service(name="service_https_transparent",
+    Service(
+        name="service_https_transparent",
         proxy_class=HttpsProxyKeybridge,
         router=TransparentRouter()
     )
 
     #ftp services
-    Service(name="service_ftp_transparent",
-            proxy_class=FtpProxyRO,
-            router=TransparentRouter()
+    Service(
+        name="service_ftp_transparent",
+        proxy_class=FtpProxyRO,
+        router=TransparentRouter()
     )
-    Service(name="service_ftp_nontransparent_inband",
-            proxy_class=FtpProxyNonTransparent,
-            router=InbandRouter(forge_port=TRUE)
+    Service(
+        name="service_ftp_nontransparent_inband",
+        proxy_class=FtpProxyNonTransparent,
+        router=InbandRouter(forge_port=TRUE)
     )
 
     #smtp services
-    Service(name="service_smtp_transparent",
+    Service(
+        name="service_smtp_transparent",
         proxy_class=SmtpProxy,
         router=TransparentRouter()
     )
-    Service(name="service_smtp_transparent_starttls",
+    Service(
+        name="service_smtp_transparent_starttls",
         proxy_class=SmtpProxyStartTls,
         router=TransparentRouter()
     )
-    Service(name="service_smtp_transparent_one_sided_ssl",
+    Service(
+        name="service_smtp_transparent_one_sided_ssl",
         proxy_class=SmtpProxyOneSideSsl,
         router=DirectedRouter(dest_addr=(SockAddrInet('172.16.20.254', 465),))
     )
 
-    Rule(service='service_http_transparent',
-         dst_port=80,
-         src_zone=('clients', ),
-         dst_zone=('servers', )
+    Rule(
+        service='service_http_transparent',
+        dst_port=80,
+        src_zone=('clients', ),
+        dst_zone=('servers', )
     )
-    Rule(service='service_http_transparent_directed',
-         dst_port=8080,
-         src_zone=('clients', )
+    Rule(
+        service='service_http_transparent_directed',
+        dst_port=8080,
+        src_zone=('clients', )
     )
-    Rule(service='service_plug',
-         dst_port=443,
-         src_zone=('clients', ),
-         dst_zone=('servers.plug', )
+    Rule(
+        service='service_plug',
+        dst_port=443,
+        src_zone=('clients', ),
+        dst_zone=('servers.plug', )
     )
-    Rule(service='service_https_transparent',
-         dst_port=443,
-         src_zone=('clients', ),
-         dst_zone=('servers', )
+    Rule(
+        service='service_https_transparent',
+        dst_port=443,
+        src_zone=('clients', ),
+        dst_zone=('servers', )
     )
-    Rule(service='service_http_transparent_header_replace',
-         dst_port=80,
-         src_zone=('clients', ),
-         dst_zone=('servers.http_header_replace', )
+    Rule(
+        service='service_http_transparent_header_replace',
+        dst_port=80,
+        src_zone=('clients', ),
+        dst_zone=('servers.http_header_replace', )
     )
-    Rule(service='service_http_transparent_url_filter',
-         dst_port=80,
-         src_zone=('clients', ),
-         dst_zone=('servers.http_url_filter', )
+    Rule(
+        service='service_http_transparent_url_filter',
+        dst_port=80,
+        src_zone=('clients', ),
+        dst_zone=('servers.http_url_filter', )
     )
-    Rule(service='service_http_nontransparent_inband',
-         dst_port=50080,
-         dst_subnet=('172.16.10.254', ),
-         src_zone=('clients', )
-    )
-
-    Rule(service='service_ftp_transparent',
-         dst_port=21,
-         src_zone=('clients', ),
-         dst_zone=('servers', )
-    )
-    Rule(service='service_ftp_nontransparent_inband',
-         dst_port=50021,
-         dst_subnet=('172.16.10.254', ),
-         src_zone=('clients', )
+    Rule(
+        service='service_http_nontransparent_inband',
+        dst_port=50080,
+        dst_subnet=('172.16.10.254', ),
+        src_zone=('clients', )
     )
 
-    Rule(service='service_smtp_transparent',
-         dst_port=25,
-         src_zone=('clients'),
-         dst_zone=('servers')
+    Rule(
+        service='service_ftp_transparent',
+        dst_port=21,
+        src_zone=('clients', ),
+        dst_zone=('servers', )
     )
-    Rule(service='service_smtp_transparent_starttls',
-         dst_port=25,
-         src_zone=('clients'),
-         dst_zone=('servers.smtp_starttls')
+    Rule(
+        service='service_ftp_nontransparent_inband',
+        dst_port=50021,
+        dst_subnet=('172.16.10.254', ),
+        src_zone=('clients', )
     )
-    Rule(service='service_smtp_transparent_one_sided_ssl',
-         dst_port=25,
-         src_zone=('clients'),
-         dst_zone=('servers.smtp_one_sided_ssl')
+
+    Rule(
+        service='service_smtp_transparent',
+        dst_port=25,
+        src_zone=('clients'),
+        dst_zone=('servers')
+    )
+    Rule(
+        service='service_smtp_transparent_starttls',
+        dst_port=25,
+        src_zone=('clients'),
+        dst_zone=('servers.smtp_starttls')
+    )
+    Rule(
+        service='service_smtp_transparent_one_sided_ssl',
+        dst_port=25,
+        src_zone=('clients'),
+        dst_zone=('servers.smtp_one_sided_ssl')
     )
 
 
 def audit_instance():
-    Service(name="service_ftp_transparent_audit",
+    Service(
+        name="service_ftp_transparent_audit",
         proxy_class=FtpProxy,
         router=TransparentRouter()
     )
-    Service(name="service_http_transparent_audit",
+    Service(
+        name="service_http_transparent_audit",
         proxy_class=HttpProxy,
         router=TransparentRouter()
     )
-    Service(name="service_pop3_transparent_audit",
+    Service(
+        name="service_pop3_transparent_audit",
         proxy_class=Pop3Proxy,
         router=TransparentRouter()
     )
-    Service(name="service_smtp_transparent_audit",
+    Service(
+        name="service_smtp_transparent_audit",
         proxy_class=SmtpProxy,
         router=TransparentRouter()
     )
 
-    Rule(service='service_ftp_transparent_audit',
-         dst_port=21,
-         src_zone=('clients', ),
-         dst_zone=('servers.audit', )
+    Rule(
+        service='service_ftp_transparent_audit',
+        dst_port=21,
+        src_zone=('clients', ),
+        dst_zone=('servers.audit', )
     )
-    Rule(service='service_http_transparent_audit',
-         dst_port=80,
-         src_zone=('clients', ),
-         dst_zone=('servers.audit', )
+    Rule(
+        service='service_http_transparent_audit',
+        dst_port=80,
+        src_zone=('clients', ),
+        dst_zone=('servers.audit', )
     )
-    Rule(service='service_pop3_transparent_audit',
-         dst_port=110,
-         src_zone=('clients', ),
-         dst_zone=('servers.audit', )
+    Rule(
+        service='service_pop3_transparent_audit',
+        dst_port=110,
+        src_zone=('clients', ),
+        dst_zone=('servers.audit', )
     )
-    Rule(service='service_smtp_transparent_audit',
-         dst_port=25,
-         src_zone=('clients', ),
-         dst_zone=('servers.audit', )
+    Rule(
+        service='service_smtp_transparent_audit',
+        dst_port=25,
+        src_zone=('clients', ),
+        dst_zone=('servers.audit', )
     )
 
 
@@ -388,40 +431,48 @@ class FtpProxyStackClamav(FtpProxy):
 
 
 def stack_instance():
-    Service(name="service_http_transparent_stack_clamav",
+    Service(
+        name="service_http_transparent_stack_clamav",
         proxy_class=HttpProxyStackClamav,
         router=TransparentRouter()
     )
-    Service(name="service_http_transparent_stack_cat",
+    Service(
+        name="service_http_transparent_stack_cat",
         proxy_class=HttpProxyStackCat,
         router=TransparentRouter()
     )
-    Service(name="service_http_transparent_stack_tr",
+    Service(
+        name="service_http_transparent_stack_tr",
         proxy_class=HttpProxyStackTr,
         router=TransparentRouter()
     )
-    Service(name="service_ftp_transparent_stack_clamav",
+    Service(
+        name="service_ftp_transparent_stack_clamav",
         proxy_class=FtpProxyStackClamav,
         router=TransparentRouter()
     )
 
-    Rule(service='service_http_transparent_stack_clamav',
-         dst_port=80,
-         src_zone=('clients', ),
-         dst_zone=('servers.stack_clamav', )
+    Rule(
+        service='service_http_transparent_stack_clamav',
+        dst_port=80,
+        src_zone=('clients', ),
+        dst_zone=('servers.stack_clamav', )
     )
-    Rule(service='service_ftp_transparent_stack_clamav',
-         dst_port=21,
-         src_zone=('clients', ),
-         dst_zone=('servers.stack_clamav', )
+    Rule(
+        service='service_ftp_transparent_stack_clamav',
+        dst_port=21,
+        src_zone=('clients', ),
+        dst_zone=('servers.stack_clamav', )
     )
-    Rule(service='service_http_transparent_stack_cat',
-         dst_port=80,
-         src_zone=('clients', ),
-         dst_zone=('servers.http_stack_cat', )
+    Rule(
+        service='service_http_transparent_stack_cat',
+        dst_port=80,
+        src_zone=('clients', ),
+        dst_zone=('servers.http_stack_cat', )
     )
-    Rule(service='service_http_transparent_stack_tr',
-         dst_port=80,
-         src_zone=('clients', ),
-         dst_zone=('servers.http_stack_tr', )
+    Rule(
+        service='service_http_transparent_stack_tr',
+        dst_port=80,
+        src_zone=('clients', ),
+        dst_zone=('servers.http_stack_tr', )
     )
